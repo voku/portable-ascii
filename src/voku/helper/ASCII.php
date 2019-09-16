5e0e67b069bf3557d5076925778ae9dda97ff7de
Lars Moelleken <lars.moelleken@vdmg-it.com>
2019-09-17 00:13:09 +0200

    [~]: "ASCII::normalize_msword()" -> merge into "ascii"-data

--- src/voku/helper/ASCII.php
+++ src/voku/helper/ASCII.php
@@ -307,43 +307,21 @@ final class ASCII
             return '';
         }
 
-        $keys = [
-            "\xc2\xab", // « (U+00AB) in UTF-8
-            "\xc2\xbb", // » (U+00BB) in UTF-8
-            "\xe2\x80\x98", // ‘ (U+2018) in UTF-8
-            "\xe2\x80\x99", // ’ (U+2019) in UTF-8
-            "\xe2\x80\x9a", // ‚ (U+201A) in UTF-8
-            "\xe2\x80\x9b", // ‛ (U+201B) in UTF-8
-            "\xe2\x80\x9c", // “ (U+201C) in UTF-8
-            "\xe2\x80\x9d", // ” (U+201D) in UTF-8
-            "\xe2\x80\x9e", // „ (U+201E) in UTF-8
-            "\xe2\x80\x9f", // ‟ (U+201F) in UTF-8
-            "\xe2\x80\xb9", // ‹ (U+2039) in UTF-8
-            "\xe2\x80\xba", // › (U+203A) in UTF-8
-            "\xe2\x80\x93", // – (U+2013) in UTF-8
-            "\xe2\x80\x94", // — (U+2014) in UTF-8
-            "\xe2\x80\xa6", // … (U+2026) in UTF-8
-        ];
+        // init
+        static $MSWORD_CACHE = [];
+        
+        if (!isset($MSWORD_CACHE['orig'])) {
+            self::prepareAsciiMaps();
 
-        $values = [
-            '"', // « (U+00AB) in UTF-8
-            '"', // » (U+00BB) in UTF-8
-            "'", // ‘ (U+2018) in UTF-8
-            "'", // ’ (U+2019) in UTF-8
-            "'", // ‚ (U+201A) in UTF-8
-            "'", // ‛ (U+201B) in UTF-8
-            '"', // “ (U+201C) in UTF-8
-            '"', // ” (U+201D) in UTF-8
-            '"', // „ (U+201E) in UTF-8
-            '"', // ‟ (U+201F) in UTF-8
-            "'", // ‹ (U+2039) in UTF-8
-            "'", // › (U+203A) in UTF-8
-            '-', // – (U+2013) in UTF-8
-            '-', // — (U+2014) in UTF-8
-            '...', // … (U+2026) in UTF-8
-        ];
+            $map = self::$ASCII_MAPS['msword'];
 
-        return \str_replace($keys, $values, $str);
+            $MSWORD_CACHE = [
+                'orig'    => \array_keys($map),
+                'replace' => \array_values($map),
+            ];
+        }
+
+        return \str_replace($MSWORD_CACHE['orig'], $MSWORD_CACHE['replace'], $str);
     }
 
     /**
